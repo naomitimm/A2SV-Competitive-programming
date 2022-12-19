9d31854a3fffc1f7a4b34c9b28262c65dbbfbe28
Naomi Timoteos <91492667+naomitimm@users.noreply.github.com>
2022-12-19 17:21:15 +0300

    Update 1.fizz_buzz.py

--- solutions/1.fizz_buzz.py
+++ solutions/1.fizz_buzz.py
@@ -1,15 +1,14 @@
-def fizzBuzz(n):
-    ls = []
-    for number in range(n):
-        if (number + 1) % 3 == 0 and (number + 1) % 5 == 0:
-            ls.append("FizzBuzz")
-        elif (number + 1) % 3 == 0:
-            ls.append("Fizz")
-        elif (number + 1) % 5 == 0:
-            ls.append("Buzz")
-        else :
-            ls.append(str(number + 1))
-    
-    print(ls)
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        ls = []
+        for number in range(n):
+            if (number + 1) % 3 == 0 and (number + 1) % 5 == 0:
+                ls.append("FizzBuzz")
+            elif (number + 1) % 3 == 0:
+                ls.append("Fizz")
+            elif (number + 1) % 5 == 0:
+                ls.append("Buzz")
+            else :
+                ls.append(str(number + 1))
 
-fizzBuzz(16)
+        return ls
