815f927576d79c67e639dd4f2d0488da864a5438
Naomi Timoteos <91492667+naomitimm@users.noreply.github.com>
2022-12-19 17:23:52 +0300

    Update 2.grading_students.py

--- solutions/2.grading_students.py
+++ solutions/2.grading_students.py
@@ -1,4 +1,19 @@
-def studentGrading(grades):
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
     result = []
     for grade in grades:
         rounder = 5 - (grade % 5)
@@ -9,10 +24,22 @@ def studentGrading(grades):
             result.append(grade)
         else:
             result.append(grade)
-    
-    print(result)
- 
+    return result
 
-number = [73, 67, 38, 33]
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-studentGrading(number)
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
