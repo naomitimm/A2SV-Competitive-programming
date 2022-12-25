c6996c142fa93b6fac1666119cbcede2bf974e08
Naomi Timothy <naomitim17.com>
2022-12-25 19:11:05 +0300

    31

---
+++ 31.min_stack.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+        if self.stack == []:
+            self.stack.append((val,val))
+            return
+
+        if val > self.stack[-1][1] :
+            self.stack.append((val, self.stack[-1][1]))  
+        else:
+            self.stack.append((val, val))    
+
+    def pop(self) -> None:
+        value = self.stack.pop()
+        return value[0]
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+        
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
